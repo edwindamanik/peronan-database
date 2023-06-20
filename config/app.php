c529099deb80af2ca90e1d1aaf5231a3782dda4b
tian0900 <iammasterofsea123@gmail.com>
2023-06-20 20:55:54 +0700

    updated

--- config/app.php
+++ config/app.php
@@ -214,6 +214,7 @@ return [
         // 'ExampleClass' => App\Example\ExampleClass::class,
 
         'Excel' => Maatwebsite\Excel\Facades\Excel::class,
+        'Request'   => Illuminate\Support\Facades\Request::class,
 
     ])->toArray(),
 
